dae3968258962562f85096160e9d8f820d38ad2b
James Hobin <hobinjk@mit.edu>
2015-07-15 20:10:31 -0700

    Add enable logic; Rename to Minimal Home Button

---
+++ minimal-home-button.js
@@ -0,0 +1,91 @@
+/* jshint browser: true, devel: true */
+
+(function() {
+
+var style = document.createElement('style');
+style.textContent = [
+':root {',
+'  --software-home-button-height: 0rem;',
+'}',
+'#software-buttons-dead-space {',
+'  display: none;',
+'  pointer-events: none;',
+'}',
+'',
+'#software-home-ring {',
+'  outline-color: #FF4E00;',
+'}',
+'',
+'#software-home-button {',
+'  position: absolute;',
+'',
+'  top: auto;',
+'  right: auto;',
+'  bottom: 0.5rem;',
+'  left: calc(50% - var(--button-width) / 2);',
+'}',
+'',
+'@media (orientation: landscape) {',
+'  #software-home-button {',
+'    top: auto;',
+'    left: auto;',
+'    right: 0.5rem;',
+'    bottom: calc(50% - var(--button-width) / 2);',
+'  }',
+'}'
+].join('\n');
+style.id = 'always-fullscreen-button-style';
+
+function handleEvent(e) {
+  var softwareHomeButton = document.getElementById('software-home-button');
+
+  switch (e.type) {
+    case 'keyboardhide':
+      softwareHomeButton.style.visibility = 'visible';
+      break;
+    // Apparently secretly keyboardshow equivalent
+    case 'keyboardchange':
+      softwareHomeButton.style.visibility = 'hidden';
+      break;
+  }
+}
+
+function setup() {
+  window.addEventListener('keyboardhide', handleEvent);
+  window.addEventListener('keyboardchange', handleEvent);
+  document.body.appendChild(style);
+}
+
+function teardown() {
+  window.removeEventListener('keyboardhide', handleEvent);
+  window.removeEventListener('keyboardchange', handleEvent);
+  document.body.removeChild(style);
+}
+
+if (document.getElementById('software-buttons')) {
+  setup();
+} else {
+  window.addEventListener('mozContentEvent', function readyListener(e) {
+    if (e.detail.type === 'system-message-listener-ready') {
+      window.removeEventListener('mozContentEvent', readyListener);
+      setup();
+    }
+  });
+}
+
+// Inspired by marumari's Sunset addon
+navigator.mozApps.mgmt.onuninstall = function() {
+  teardown();
+};
+navigator.mozApps.mgmt.onenabledstatechange = function(event) {
+  var app = event.application;
+  if (app.manifest.name === 'Minimal Home Button') {
+    if (app.enabled) {
+      setup();
+    } else {
+      teardown();
+    }
+  }
+};
+
+})();
